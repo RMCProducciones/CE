cec54a4c5279fa948bc6f25c614e3478bcd56329
jarocha <jhonyrocha@twoboxes.com>
2015-11-13 00:28:38 -0500

    Resolviendo conflicto entre AngularJS y Twig

--- web/public/js/scripts.js
+++ web/public/js/scripts.js
@@ -1,9 +1,27 @@
 
 $('[data-toggle=confirmation]').confirmation();
 
-var app = angular.module('aplicationCE', []);
-
-app.controller('controllerGrupo', function($scope) {
-	$scope.count = -1;
+var app = angular.module('aplicationCE', []).config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('[[').endSymbol(']]');
 });
+
+app.constant('styleBuscarHerramientas', { dropdown: 'dropdown', dropup: 'dropup' });
+
+app.controller('buttonBuscarHerramientas', ['$scope', 'styleBuscarHerramientas',function($scope, styleBuscarHerramientas) {
+	$scope.count = -1;
+	$scope.styleBuscarHerramientas = styleBuscarHerramientas.dropdown;
+	
+	$scope.buttonBuscarHerramientas = function(count){
+		$scope.count = count * (-1);
+		if($scope.count== -1)
+		{
+			$scope.styleBuscarHerramientas = styleBuscarHerramientas.dropdown;
+		}
+		else
+		{
+			$scope.styleBuscarHerramientas = styleBuscarHerramientas.dropup;
+		}
+    }
+	
+}]);
 			
