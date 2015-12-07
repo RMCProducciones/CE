475e4e0cb4a5155078ced8f7d1d53cf844eeef9a
jbolanosrmc <j.bolanos@rmcproducciones.com>
2015-12-07 16:03:23 -0500

    Beca Entity

---
+++ pendientes.sh
@@ -0,0 +1,3 @@
+php app/console doctrine:generate:entity --entity="AppBundle:Beca" --fields="fecha_publicacion:datetime fecha_inicio:datetime fecha_finalizacion:datetime nombre:string tipo:integer modalidad:integer municipio:integer active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
+
+php app/console doctrine:generate:entity --entity="AppBundle:AsignacionUsuarioBeca" --fields="usuario:integer fecha_asignacion:datetime estado:integer observaciones:string active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
---
+++ src/AppBundle/Entity/Beca.php
@@ -0,0 +1,407 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Beca
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Beca
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_publicacion", type="datetime")
+     */
+    private $fecha_publicacion;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_inicio", type="datetime")
+     */
+    private $fecha_inicio;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_finalizacion", type="datetime")
+     */
+    private $fecha_finalizacion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string")
+     */
+    private $nombre;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="tipo", type="integer")
+     */
+    private $tipo;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="modalidad", type="integer")
+     */
+    private $modalidad;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="municipio", type="integer")
+     */
+    private $municipio;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="usuario_modificacion", type="integer")
+     */
+    private $usuario_modificacion;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_modificacion", type="datetime")
+     */
+    private $fecha_modificacion;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="usuario_creacion", type="integer")
+     */
+    private $usuario_creacion;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_creacion", type="datetime")
+     */
+    private $fecha_creacion;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fechaPublicacion
+     *
+     * @param \DateTime $fechaPublicacion
+     *
+     * @return Beca
+     */
+    public function setFechaPublicacion($fechaPublicacion)
+    {
+        $this->fecha_publicacion = $fechaPublicacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaPublicacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaPublicacion()
+    {
+        return $this->fecha_publicacion;
+    }
+
+    /**
+     * Set fechaInicio
+     *
+     * @param \DateTime $fechaInicio
+     *
+     * @return Beca
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fecha_inicio = $fechaInicio;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaInicio
+     *
+     * @return \DateTime
+     */
+    public function getFechaInicio()
+    {
+        return $this->fecha_inicio;
+    }
+
+    /**
+     * Set fechaFinalizacion
+     *
+     * @param \DateTime $fechaFinalizacion
+     *
+     * @return Beca
+     */
+    public function setFechaFinalizacion($fechaFinalizacion)
+    {
+        $this->fecha_finalizacion = $fechaFinalizacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaFinalizacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaFinalizacion()
+    {
+        return $this->fecha_finalizacion;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Beca
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set tipo
+     *
+     * @param integer $tipo
+     *
+     * @return Beca
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get tipo
+     *
+     * @return integer
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set modalidad
+     *
+     * @param integer $modalidad
+     *
+     * @return Beca
+     */
+    public function setModalidad($modalidad)
+    {
+        $this->modalidad = $modalidad;
+
+        return $this;
+    }
+
+    /**
+     * Get modalidad
+     *
+     * @return integer
+     */
+    public function getModalidad()
+    {
+        return $this->modalidad;
+    }
+
+    /**
+     * Set municipio
+     *
+     * @param integer $municipio
+     *
+     * @return Beca
+     */
+    public function setMunicipio($municipio)
+    {
+        $this->municipio = $municipio;
+
+        return $this;
+    }
+
+    /**
+     * Get municipio
+     *
+     * @return integer
+     */
+    public function getMunicipio()
+    {
+        return $this->municipio;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     *
+     * @return Beca
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * Set usuarioModificacion
+     *
+     * @param integer $usuarioModificacion
+     *
+     * @return Beca
+     */
+    public function setUsuarioModificacion($usuarioModificacion)
+    {
+        $this->usuario_modificacion = $usuarioModificacion;
+
+        return $this;
+    }
+
+    /**
+     * Get usuarioModificacion
+     *
+     * @return integer
+     */
+    public function getUsuarioModificacion()
+    {
+        return $this->usuario_modificacion;
+    }
+
+    /**
+     * Set fechaModificacion
+     *
+     * @param \DateTime $fechaModificacion
+     *
+     * @return Beca
+     */
+    public function setFechaModificacion($fechaModificacion)
+    {
+        $this->fecha_modificacion = $fechaModificacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaModificacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaModificacion()
+    {
+        return $this->fecha_modificacion;
+    }
+
+    /**
+     * Set usuarioCreacion
+     *
+     * @param integer $usuarioCreacion
+     *
+     * @return Beca
+     */
+    public function setUsuarioCreacion($usuarioCreacion)
+    {
+        $this->usuario_creacion = $usuarioCreacion;
+
+        return $this;
+    }
+
+    /**
+     * Get usuarioCreacion
+     *
+     * @return integer
+     */
+    public function getUsuarioCreacion()
+    {
+        return $this->usuario_creacion;
+    }
+
+    /**
+     * Set fechaCreacion
+     *
+     * @param \DateTime $fechaCreacion
+     *
+     * @return Beca
+     */
+    public function setFechaCreacion($fechaCreacion)
+    {
+        $this->fecha_creacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaCreacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fecha_creacion;
+    }
+}
+
