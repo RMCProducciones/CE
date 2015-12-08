1e98ef851e65e4fec1a0aa9afd30545a7f3677e7
jbolanosrmc <j.bolanos@rmcproducciones.com>
2015-12-08 11:11:33 -0500

    Poliza

--- generacionEntities.sh generados.sh
+++ generacionEntities.sh generados.sh
@@ -59,5 +59,9 @@ php app/console doctrine:generate:entity --entity="AppBundle:AsignacionBeneficia
 php app/console doctrine:generate:entity --entity="AppBundle:SeguimientoBeneficiarioAhorro" --fields="beneficiario:integer fecha_apertura:datetime saldo_apertura:decimal incentivo_apertura:decimal fecha_corte_1:datetime saldo_corte_1:decimal incentivo_corte_1:decimal fecha_corte_2:datetime saldo_corte_2:decimal incentivo_corte_2:decimal fecha_corte_final:datetime saldo_corte_final:decimal incentivo_corte_final:decimal numero_incumplimiento:integer active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
 
 
-php app/console doctrine:generate:entity --entity="AppBundle:PolizaSoporte" --fields="grupo:integer tipo_soporte:integer estado:integer consecutivo:integer cofinanciacion:decimal path:string active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
 
+php app/console doctrine:generate:entity --entity="AppBundle:Poliza" --fields="grupo:integer active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
+
+php app/console doctrine:generate:entity --entity="AppBundle:PolizaSoporte" --fields="poliza:integer tipo_soporte:integer estado:integer consecutivo:integer cofinanciacion:decimal path:string active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
+
+php app/console doctrine:generate:entity --entity="AppBundle:AsignacionBeneficiarioPoliza" --fields="poliza:integer beneficiario:integer beneficiario_poliza_otro_programa:boolean observaciones:string active:boolean usuario_modificacion:integer fecha_modificacion:datetime usuario_creacion:integer fecha_creacion:datetime"
