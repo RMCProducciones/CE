9f86cb794924c7297c5f74064055558f4dd45267
jarocha <jhonyrocha@twoboxes.com>
2016-01-05 10:20:21 -0500

    Programando lógica de roles, perfiles y permisos desde un archivo xml

--- web/public/js/default.js
+++ web/public/js/default.js
@@ -26,3 +26,32 @@ app.controller('FiltrosCtrl', ['$scope', '$http', 'styleBuscarHerramientas', fun
     }	
 		
 }]);
+
+app.controller('MenuCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.RPP = '';
+
+	$http({
+    	method  : 'GET',
+    	url     : $scope.rutaServidor + "public/xml/menu.xml",
+    	timeout : 10000,
+    	params  : {},  // Query Parameters (GET)
+    	transformResponse : function(data) {
+        	// string -> XML document object
+        	var x2js = new X2JS();
+            var json = x2js.xml_str2json(data);
+
+            return json;
+    	}
+	}).success(function(data, status, headers, config) {
+    
+		var array = data == null ? [] : (data.structure.component instanceof Array ? data.structure.component : [data.structure.component]);
+
+		$scope.RPP = array;
+
+		console.log($scope.RPP);
+	
+	}).error(function(data, status, headers, config) {
+		console.log("Problemas al cargar la estructura del mapa de navegación de los usuarios");
+    	//alert('Problemas a');
+	});
+}]);
--- web/public/js/localizacion.js
+++ web/public/js/localizacion.js
@@ -105,6 +105,9 @@ function obtenerDepartamento($http, $scope){
 	$http.get($scope.rutaServidor + "departamentos")
 	.success(function(data) {
 
+		//console.log("departamento: " + data);
+		//console.log(data);
+
 		var array = data == null ? [] : (data instanceof Array ? data : [data]);
 
 		if($scope.nuevoRegistro == true){
@@ -135,6 +138,8 @@ function obtenerZona($http, $scope){
 		
 		$scope.JSONZona  = array;
 		$scope.selZona   = $scope.JSONZona;
+
+		//console.log(array);
 
 		obtenerMunicipio($http, $scope);
 
