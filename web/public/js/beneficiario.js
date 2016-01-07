7c0ca8849c5b7933ab91768f2c5f162eeca5ee1e
juancantor <juanc888@hotmail.com>
2016-01-07 09:17:06 -0500

    merge

--- web/public/js/beneficiario.js
+++ web/public/js/beneficiario.js
@@ -3,7 +3,7 @@ app.controller('GrupoIndigenaCtrl', ['$scope', function($scope) {
 	$scope.mostrarPertenenciaEtnica = false;
 
 	$scope.pertenenciaEtnica = function() { 
-		$scope.mostrarPertenenciaEtnica = $(".grupo_indigena option[value='"+$('.grupo_indigena').val()+"']").text().toLowerCase() == "indigena";			
+		$scope.mostrarPertenenciaEtnica = $(".grupo_indigena option[value='"+$('.grupo_indigena').val()+"']").text().toLowerCase() == "dujo";			
 	};	
 
 }]);
