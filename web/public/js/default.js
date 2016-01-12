add24c1b6ccc61d6d3c0d23b6c1013475ba983ce
jarocha <jhonyrocha@twoboxes.com>
2016-01-12 14:25:06 -0500

    Programando cargue y display del menu con AngularJS

--- web/public/js/default.js
+++ web/public/js/default.js
@@ -99,7 +99,10 @@ app.controller('PermisoCtrl', ['$scope', '$http', function($scope, $http) {
 			var elementComponent = {};
 			var arrayModule = [];
 
-			elementComponent.code = (idObjComponent + 1);
+			elementComponent.id = (idObjComponent + 1);
+			elementComponent.code = $(objComponent).attr("component-code");
+			elementComponent.path = $(objComponent).attr("component-path");
+			elementComponent.title = $(objComponent).attr("component-title");
 
 			//Para los módulos
 			$.each($('.component .module-' + (idObjComponent+1)), function( idObjModule, objModule ) {
@@ -107,7 +110,10 @@ app.controller('PermisoCtrl', ['$scope', '$http', function($scope, $http) {
 				var elementModule = {};
 				var arraySubModule = [];
 
-				elementModule.code = (idObjModule + 1);
+				elementModule.id = (idObjModule + 1);
+				elementModule.code = $(objModule).attr("module-code");
+				elementModule.path = $(objModule).attr("module-path");
+				elementModule.title = $(objModule).attr("module-title");
 
 				//Para los submódulos
 				$.each($('.component .module-' + (idObjComponent+1) + ' .submodule-' + (idObjComponent+1) + '-' + (idObjModule+1)), function( idObjSubModule, objSubModule ) {
@@ -115,14 +121,17 @@ app.controller('PermisoCtrl', ['$scope', '$http', function($scope, $http) {
 						var elementSubModule = {};
 						var arrayAction = [];
 
-						elementSubModule.code = (idObjSubModule + 1);
+						elementSubModule.id = (idObjSubModule + 1);
+						elementSubModule.code = $(objSubModule).attr("submodule-code");
+						elementSubModule.path = $(objSubModule).attr("submodule-path");
+						elementSubModule.title = $(objSubModule).attr("submodule-title");
 
 						//Para las funciones o acciones
 						$.each($('.component .module-' + (idObjComponent+1) + ' .submodule-' + (idObjComponent+1) + '-' + (idObjModule+1) + ' .ckPermiso-' + (idObjComponent+1) + '-' + (idObjModule+1) + '-' + (idObjSubModule+1) ), function( idObjFuncionalidad, objFuncionalidad ) {
 
 							var elementAction = {};
 
-							elementAction.code = (idObjFuncionalidad + 1);
+							elementAction.id = (idObjFuncionalidad + 1);
 							elementAction.checked = $('.component .module-' + (idObjComponent+1) + ' .submodule-' + (idObjComponent+1) + '-' + (idObjModule+1) + ' #ckPermiso-' + (idObjComponent+1) + '-' + (idObjModule+1) + '-' + (idObjSubModule+1) + '-' + (idObjFuncionalidad+1))[0].checked;
 
 							arrayAction.push(elementAction);
@@ -149,7 +158,7 @@ app.controller('PermisoCtrl', ['$scope', '$http', function($scope, $http) {
 
 		$scope.elementPermiso = JSON.stringify(elementPermiso);
 
-		//console.log($scope.elementPermiso);
+		console.log($scope.elementPermiso);
 
     }	
 
@@ -157,7 +166,7 @@ app.controller('PermisoCtrl', ['$scope', '$http', function($scope, $http) {
 
 app.controller('MenuCtrl', ['$scope', '$http', function($scope, $http) {
 
-	$scope.pruebaJsonMenu = '{"component":[{"code":1,"module":[{"code":1,"subModule":[{"code":1,"action":[{"code":1,"checked":true},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":2,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":3,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]}]},{"code":2,"subModule":[{"code":1,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":2,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":3,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]}]},{"code":3,"subModule":[{"code":1,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":2,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":3,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":4,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]},{"code":5,"action":[{"code":1,"checked":false},{"code":2,"checked":false},{"code":3,"checked":false},{"code":4,"checked":false}]}]}]}]}';
+	$scope.pruebaJsonMenu = '{"component":[{"id":1,"code":"1","path":"#","title":"Gestion Empresarial","module":[{"id":1,"code":"1","path":"#","title":"Formacion de capital social asociativo y desarrollo empresarial","subModule":[{"id":1,"code":"1","path":"/gestion-empresarial/desarrollo-empresarial/grupos/","title":"Gestión de Grupos","action":[{"id":1,"checked":true},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"CLEARGestion","title":"Gestión de CLEAR","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Gestión de seguimiento y monitoreo","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]},{"id":2,"code":"2","path":"#","title":"Concursos de mejoramiento","subModule":[{"id":1,"code":"1","path":"concursoGestion","title":"Gestion de Concursos","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"#","title":"Gestion de Jurados","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Gestion de seguimiento y monitoreo","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]},{"id":3,"code":"3","path":"#","title":"Servicios complementarios","subModule":[{"id":1,"code":"1","path":"#","title":"Participación rutas y pasantias","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"#","title":"Participación en talleres","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Participación en Ferias","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":4,"code":"4","path":"#","title":"Participación en ferias de difusión del proyecto","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":5,"code":"5","path":"#","title":"Desarrollo de ferias de difusión del proyecto","action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]}]}]}';
 	$scope.pruebaJsonMenu = angular.fromJson($scope.pruebaJsonMenu);
 
 	console.log($scope.pruebaJsonMenu);
@@ -165,6 +174,5 @@ app.controller('MenuCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.construirMenuUsuario = function(){
 
     }
-
 
 }]);	
