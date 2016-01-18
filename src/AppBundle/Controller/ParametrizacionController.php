66e67c6055aff12f4bad752f09b08a78c3f0b3d8
jarocha <jhonyrocha@twoboxes.com>
2016-01-18 11:08:14 -0500

    Programando rutas del controlador de gestion de roles

--- src/AppBundle/Controller/ParametrizacionController.php
+++ src/AppBundle/Controller/ParametrizacionController.php
@@ -22,9 +22,9 @@ class ParametrizacionController extends Controller
 
 
     /**
-     * @Route("/backend/permiso-rol/gestion", name="gestionPermisoRol")
+     * @Route("/backend/rol/gestion", name="gestionRol")
      */
-    public function gestionPermisoRolAction()
+    public function gestionRolAction()
     {
 
 
@@ -42,20 +42,9 @@ class ParametrizacionController extends Controller
 
 
     /**
-     * @Route("/backend/permiso-rol/asignar/{idUsuario}", name="asignarPermisoRol")
+     * @Route("/backend/rol/editar/{idRol}", name="rolEditar")
      */
-    public function asignarPermisoRolAction($idUsuario)
-    {
-
-        return $this->render(
-        	'AppBundle:Backend/Parametrizacion:permiso-rol-asignar.html.twig' 
-        );
-    }
-
-    /**
-     * @Route("/backend/permiso-rol/editar/{idUsuario}", name="editarPermisoRol")
-     */
-    public function editarPermisoRolAction($idUsuario)
+    public function rolEditarAction($idRol)
     {
 
         $permisoRol = '{"component":[{"id":1,"code":"1","path":"#","title":"Gestion Empresarial","checked":true,"module":[{"id":1,"code":"1","path":"#","title":"Formacion de capital social asociativo y desarrollo empresarial","checked":true,"subModule":[{"id":1,"code":"1","path":"gruposGestion","title":"Gestión de Grupos","checked":true,"action":[{"id":1,"checked":true},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"CLEARGestion","title":"Gestión de CLEAR","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Gestión de seguimiento y monitoreo","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]},{"id":2,"code":"2","path":"#","title":"Concursos de mejoramiento","checked":false,"subModule":[{"id":1,"code":"1","path":"concursoGestion","title":"Gestion de Concursos","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"#","title":"Gestion de Jurados","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Gestion de seguimiento y monitoreo","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]},{"id":3,"code":"3","path":"#","title":"Servicios complementarios","checked":false,"subModule":[{"id":1,"code":"1","path":"#","title":"Participación rutas y pasantias","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":2,"code":"2","path":"#","title":"Participación en talleres","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":3,"code":"3","path":"#","title":"Participación en Ferias","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":4,"code":"4","path":"#","title":"Participación en ferias de difusión del proyecto","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]},{"id":5,"code":"5","path":"#","title":"Desarrollo de ferias de difusión del proyecto","checked":false,"action":[{"id":1,"checked":false},{"id":2,"checked":false},{"id":3,"checked":false},{"id":4,"checked":false}]}]}]}]}';        
@@ -75,8 +64,6 @@ class ParametrizacionController extends Controller
         $elementos = $query->getResult();
         */
 
-//        $securityContext = $this->get('security.context');
-        
         $jsonPermisoRol = json_decode($permisoRol,true);
 
 
