f59bd5658700e55a65b35aee76de7c9bf432c135
juancantor <juanc888@hotmail.com>
2016-01-28 11:53:25 -0500

    menu modificado

--- src/AppBundle/Controller/ParametrizacionController.php
+++ src/AppBundle/Controller/ParametrizacionController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 use AppBundle\Entity\Rol;
+use AppBundle\Entity\Usuario;
 
 use AppBundle\Form\Backend\Parametrizacion\RolType;
 
@@ -185,6 +186,32 @@ class ParametrizacionController extends Controller
         return new Response($permisoRol);
 
     }
+
+
+
+    /**
+     * @Route("/parametrizacion/usuario", name="usuarioGestion")
+     */
+    public function usuarioGestionAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+       
+        $usuarios = $em->getRepository('AppBundle:Usuario')->findBY(
+            array('active' => 1)         
+        );
+
+
+        
+        return $this->render(
+            'AppBundle:GestionParametro:usuario-gestion.html.twig', 
+            array( 
+                'usuarios' => $usuarios
+            )
+        );
+
+    }
+
 
 
 }
