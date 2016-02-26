4511fcfe598460320a30bba41d792d8e631fa797
juancantor <juanc888@hotmail.com>
2016-02-26 13:10:02 -0500

    Experiencia Exitosa

--- src/AppBundle/Controller/GestionConocimientoController.php
+++ src/AppBundle/Controller/GestionConocimientoController.php
@@ -16,6 +16,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 use AppBundle\Entity\ExperienciaExitosa;
+use AppBundle\Entity\ExperienciaExitosaSoporte;
 use AppBundle\Entity\Talento;
 use AppBundle\Entity\TalentoSoporte;
 use AppBundle\Entity\Beca;
@@ -24,6 +25,7 @@ use AppBundle\Entity\Evento;
 
 
 use AppBundle\Form\GestionConocimiento\ExperienciaExitosaType;
+use AppBundle\Form\GestionConocimiento\ExperienciaExitosaSoporteType;
 use AppBundle\Form\GestionConocimiento\TalentoType;
 use AppBundle\Form\GestionConocimiento\TalentoSoporteType;
 use AppBundle\Form\GestionConocimiento\BecaType;
@@ -60,7 +62,7 @@ class GestionConocimientoController extends Controller
     public function experienciaNuevoAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $experienciaexitosa = new Experienciaexitosa();
+        $experienciaexitosa = new ExperienciaExitosa();
         
         $form = $this->createForm(new ExperienciaExitosaType(), $experienciaexitosa);
 
@@ -83,8 +85,185 @@ class GestionConocimientoController extends Controller
         
         return $this->render('AppBundle:GestionConocimiento:experiencia-nuevo.html.twig', array('form' => $form->createView()));
     } 
+
+    /**
+     * @Route("/gestion-conocimiento/experiencia-exitosa/{idExperienciaExitosa}/editar", name="experienciaEditar")
+     */
+    public function experienciaEditarAction(Request $request, $idExperienciaExitosa)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $experienciaexitosa = new ExperienciaExitosa();
+
+        $experienciaexitosa = $em->getRepository('AppBundle:ExperienciaExitosa')->findOneBy(
+            array('id' => $idExperienciaExitosa)
+        );
+
+        $form = $this->createForm(new ExperienciaExitosaType(), $experienciaexitosa);
+        
+        $form->add(
+            'Guardar', 
+            'submit', 
+            array(
+                'attr' => array(
+                    'style' => 'visibility:hidden'
+                ),
+            )
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $experienciaexitosa = $form->getData();
+
+            $experienciaexitosa->setFechaModificacion(new \DateTime());
+
+            
+
+            $em->flush();
+
+            return $this->redirectToRoute('experienciaGestion');
+        }
+
+        return $this->render(
+            'AppBundle:GestionConocimiento:experiencia-editar.html.twig', 
+            array(
+                    'form' => $form->createView(),
+                    'idExperienciaExitosa' => $idExperienciaExitosa,
+                    'experienciaexitosa' => $experienciaexitosa,
+            )
+        );
+
+    }
+
+
+
+    /**
+     * @Route("/gestion-conocimiento/experiencia-exitosa/{idExperienciaExitosa}/eliminar", name="experienciaEliminar")
+     */
+    public function experienciaEliminarAction(Request $request, $idExperienciaExitosa)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $experienciaexitosa = new ExperienciaExitosa();
+
+        $experienciaexitosa = $em->getRepository('AppBundle:ExperienciaExitosa')->find($idExperienciaExitosa);              
+
+        $em->remove($experienciaexitosa);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('experienciaGestion'));
+
+    }
+
+    /**
+     * @Route("/gestion-conocimiento/experiencia-exitosa/{idExperienciaExitosa}/documentos-soporte", name="experienciaSoporte")
+     */
+    public function experienciaSoporteAction(Request $request, $idExperienciaExitosa)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $experienciaexitosaSoporte = new ExperienciaExitosaSoporte();
+        
+        $form = $this->createForm(new ExperienciaExitosaSoporteType(), $experienciaexitosaSoporte);
+
+        $form->add(
+            'Guardar', 
+            'submit', 
+            array(
+                'attr' => array(
+                    'style' => 'visibility:hidden'
+                ),
+            )
+        );
+
+        $soportesActivos = $em->getRepository('AppBundle:ExperienciaExitosaSoporte')->findBy(
+            array('active' => '1', 'experienciaexitosa' => $idExperienciaExitosa),
+            array('fecha_creacion' => 'ASC')
+        );
+
+        $histotialSoportes = $em->getRepository('AppBundle:ExperienciaExitosaSoporte')->findBy(
+            array('active' => '0', 'experienciaexitosa' => $idExperienciaExitosa),
+            array('fecha_creacion' => 'ASC')
+        );
+        
+        $experienciaexitosa = $em->getRepository('AppBundle:ExperienciaExitosa')->findOneBy(
+            array('id' => $idExperienciaExitosa)
+        );
+        
+        if ($this->getRequest()->isMethod('POST')) {
+            $form->bind($this->getRequest());
+            if ($form->isValid()) {
+
+                $tipoSoporte = $em->getRepository('AppBundle:DocumentoSoporte')->findOneBy(
+                    array(
+                        'descripcion' => $talentoSoporte->getTipoSoporte()->getDescripcion(), 
+                        'dominio' => 'talento_tipo_soporte'
+                    )
+                );
+                
+                $actualizarExperienciaExitosaSoportes = $em->getRepository('AppBundle:ExperienciaExitosaSoporte')->findBy(
+                    array(
+                        'active' => '1' , 
+                        'tipo_soporte' => $tipoSoporte->getId(), 
+                        'experienciaexitosa' => $idExperienciaExitosa
+                    )
+                );  
+            
+                foreach ($actualizarExperienciaExitosaSoportes as $actualizarExperienciaExitosaSoporte){
+                    echo $actualizarExperienciaExitosaSoporte->getId()." ".$actualizarExperienciaExitosaSoporte->getTipoSoporte()."<br />";
+                    $actualizarExperienciaExitosaSoporte->setFechaModificacion(new \DateTime());
+                    $actualizarExperienciaExitosaSoporte->setActive(0);
+                    $em->flush();
+                }
+                
+                $experienciaexitosaSoporte->setExperienciaExitosa($experienciaexitosa);
+                $experienciaexitosaSoporte->setActive(true);
+                $experienciaexitosaSoporte->setFechaCreacion(new \DateTime());
+                //$grupoSoporte->setUsuarioCreacion(1);
+
+                $em->persist($experienciaexitosaSoporte);
+                $em->flush();
+
+                return $this->redirectToRoute('experienciaSoporte', array( 'idExperienciaExitosa' => $idExperienciaExitosa));
+            }
+        }   
+        
+        return $this->render(
+            'AppBundle:GestionConocimiento:experiencia-soporte.html.twig', 
+            array(
+                'form' => $form->createView(), 
+                'soportesActivos' => $soportesActivos, 
+                'histotialSoportes' => $histotialSoportes
+            )
+        );
+        
+    }
+    
+    /**
+     * @Route("/gestion-conocimiento/experiencia-exitosa/{idExperienciaExitosa}/documentos-soporte/{idExperienciaExitosaSoporte}/borrar", name="experienciaSoporteBorrar")
+     */
+    public function experienciaSoporteBorrarAction(Request $request, $idExperienciaExitosa, $idExperienciaExitosaSoporte)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $experienciaexitosaSoporte = new ExperienciaExitosaSoporte();
+        
+        $experienciaexitosaSoporte = $em->getRepository('AppBundle:ExperienciaExitosaSoporte')->findOneBy(
+            array('id' => $idExperienciaExitosaSoporte)
+        );
+        
+        $experienciaexitosaSoporte->setFechaModificacion(new \DateTime());
+        $experienciaexitosaSoporte->setActive(0);
+        $em->flush();
+
+        return $this->redirectToRoute('experienciaSoporte', array( 'idExperienciaExitosa' => $idExperienciaExitosa));
+        
+    }
    
    
+
+
+
    /**
      * @Route("/gestion-conocimiento/talento", name="talentoGestion")
      */
