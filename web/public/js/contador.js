123d35d94c3ba76c1726b7a93ca82609512437f8
juancantor <juanc888@hotmail.com>
2016-03-15 08:59:24 -0500

    tarea realizada

---
+++ web/public/js/contador.js
@@ -0,0 +1,52 @@
+
+app.controller('gestionDocumentoSoporteContadorCtrl', ['$scope', '$location', function($scope, $location) {
+
+	$scope.idContador = 0;
+	$scope.idContadorSoporteActivo = 0;
+
+	$scope.anularSoporteContador = function() { 
+
+		window.location.replace($scope.rutaServidor  + "gestion-empresarial/desarrollo-empresarial/contador/" + $scope.idContador + "/documentos-soporte/" + $scope.idContadorSoporteActivo + "/borrar");
+		
+	};
+
+
+}]);
+
+
+app.controller('gestionContadorCtrl', ['$scope', '$http', function($scope, $http) {
+
+	$scope.idContador = 0;	
+
+	console.log($scope.estadoMensaje);
+
+	$scope.eliminarContador = function(idGrupo, idContador, consecutivo){
+
+		$scope.idGrupo = idGrupo;
+		$scope.idContador = idContador;
+		$scope.consecutivoContador = consecutivo;
+
+		$http.get($scope.rutaServidor + "gestion-empresarial/desarrollo-empresarial/grupo/" +$scope.idGrupo + "/contador/" + $scope.idContador + "/eliminar")
+		.success(function(data, status, headers, config) {
+
+  			$("#filaContador" + $scope.consecutivoContador).fadeOut("slow");
+			$scope.mostrarMensaje("success", true, "Registro Eliminado");
+
+		
+		}).error(function(data, status, headers, config) {
+
+			console.log($(data).filter("title").html());
+
+			$scope.mostrarMensaje("warning", true, $(data).filter("title").html());
+
+			
+		});
+
+		
+	};	
+
+}]);
+
+
+
+
