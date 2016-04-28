6b001eeb682dca2f23c3879f4149cf921fa628a4
juancantor <juanc888@hotmail.com>
2016-04-28 12:25:20 -0500

    Concurso

--- src/AppBundle/Form/GestionEmpresarial/AprobacionConcursoType.php
+++ src/AppBundle/Form/GestionEmpresarial/AprobacionConcursoType.php
@@ -13,8 +13,14 @@ class AprobacionConcursoType extends AbstractType
         $builder
 		
 			->add('aprobacion')
+			->add('aprobador')
+			->add('observaciones')
 			->add('fecha_inicio', 'date', array('label' => 'Fecha de Inicio del concurso', 'widget' => 'single_text'))
-			->add('fecha_finalizacion', 'date', array('label' => 'Fecha de finalizacion del concurso', 'widget' => 'single_text'));
+			->add('fecha_finalizacion', 'date', array('label' => 'Fecha de finalizacion del concurso', 'widget' => 'single_text'))
+			->add('fecha_inicio_propuesta', 'date', array('label' => 'Fecha de Inicio del concurso Propuesta', 'widget' => 'single_text'))
+			->add('fecha_finalizacion_propuesta', 'date', 
+			array('label' => 'Fecha de finalizacion del concurso Propuesta', 'widget' => 'single_text'))
+			;
 			
 	}		
 			
