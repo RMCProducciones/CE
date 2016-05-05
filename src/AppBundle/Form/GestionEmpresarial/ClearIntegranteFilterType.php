75b6ffedadcce4cc9a14904c98e13e345ee80410
cdanielb <cadan95@gmail.com>
2016-05-05 08:48:41 -0500

    Solucion de errores

--- src/AppBundle/Form/GestionEmpresarial/ClearIntegranteFilterType.php
+++ src/AppBundle/Form/GestionEmpresarial/ClearIntegranteFilterType.php
@@ -14,7 +14,7 @@ class ClearIntegranteFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     { 
-        $builder->add('nombre', 'filter_text', array('label' => 'Nombre'));
+        $builder->add('primer_nombre', 'filter_text', array('label' => 'Primer Nombre'));
         $builder->add('primer_apellido', 'filter_text', array('label' => 'Primer Apellido'));
         $builder->add('numero_documento', 'filter_text', array('label' => 'Número de Documento'));
 
