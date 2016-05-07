d98f56aae10bbd309aa402321ad5f01a8f376181
jarocha <jhonyrocha@twoboxes.com>
2016-05-06 19:48:15 -0500

    Solucionando problema de descargar archivo en grupo gestión

--- src/AppBundle/Controller/GestionEmpresarial/GrupoController.php
+++ src/AppBundle/Controller/GestionEmpresarial/GrupoController.php
@@ -8,12 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-
 
 use AppBundle\Entity\Grupo;
 use AppBundle\Entity\Beneficiario;
