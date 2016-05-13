202ebedda3f813ef734619d96c4935aeb5006c02
cdanielb <cadan95@gmail.com>
2016-05-13 16:14:04 -0500

    Descarga de Documentos, prueba en servidor 1

--- src/AppBundle/Controller/GestionEmpresarial/ClearController.php
+++ src/AppBundle/Controller/GestionEmpresarial/ClearController.php
@@ -339,11 +339,11 @@ class ClearController extends Controller
 
         $link = '..\uploads\documents\\'.$path->getPath();
 
-        header("Content-Disposition: attachment; filename = $link");
+        /*header("Content-Disposition: attachment; filename = $link");
         header ("Content-Type: application/force-download");
         header ("Content-Length: ".filesize($link));
-        readfile($link);           
-        //return new BinaryFileResponse($link); -> para mostrar en ventana aparte
+        readfile($link);           */
+        return new BinaryFileResponse($link); //-> para mostrar en ventana aparte
     }
 
     /**
